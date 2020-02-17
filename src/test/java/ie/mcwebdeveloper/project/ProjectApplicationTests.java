e1bfb0fd95534c37aa497577b7c591cf9e363e24
Sam O'Neill <sam.oneill1@ucdconnect.ie>
2020-02-17 18:31:39 +0000

    Initial project set-up and basic aesthetics

---
+++ src/test/java/ie/mcwebdeveloper/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.mcwebdeveloper.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
