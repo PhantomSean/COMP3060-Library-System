b19a29941bedff01f5abbc20926472c22a72683d
dearbhlamceniff <dearbhlamceniff@gmail.com>
2020-03-03 13:27:01 +0000

    created profile page

--- src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
+++ src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
@@ -1,11 +1,19 @@
 package ie.mcwebdeveloper.project.controller;
 
+import ie.mcwebdeveloper.project.models.Member;
+import ie.mcwebdeveloper.project.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class LibraryController {
-
+    @Autowired
+    MemberRepository repository;
     // Index
     @GetMapping("/")
     public String getIndex() {
@@ -35,5 +43,11 @@ public class LibraryController {
     public String getSearch() {
         return "search.html";
     }
-
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable String id, Model model){
+        long i = Long.parseLong(id);
+        Member member = repository.findById(i);
+        model.addAttribute("member", member);
+        return "profile.html";
+    }
 }
