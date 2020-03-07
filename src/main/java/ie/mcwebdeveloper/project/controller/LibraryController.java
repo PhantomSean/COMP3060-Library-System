d978ba408969487b7225f67811053480d03b9f43
PhantomSean <sean.stewart@ucdconnect.ie>
2020-03-07 14:09:34 +0000

    Change to profile html

--- src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
+++ src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -39,10 +40,12 @@ public class LibraryController {
     public String getSignup() {
         return "signup.html";
     }
+    // search
     @GetMapping("/search")
     public String getSearch() {
         return "search.html";
     }
+
     @GetMapping("/profile/{id}")
     public String profile(@PathVariable String id, Model model){
         long i = Long.parseLong(id);
