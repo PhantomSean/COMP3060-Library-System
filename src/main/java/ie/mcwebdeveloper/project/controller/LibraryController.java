456c01626662f509bd64474d9cf46e465f104f87
Sam <sam.oneill1@ucdconnect.ie>
2020-03-20 15:39:26 +0000

    Update LibraryController.java

--- src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
+++ src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
@@ -35,76 +35,6 @@ public class LibraryController {
         return "index.html";
     }
 
-    @GetMapping("/login")
-    public String getLogin() {
-        return "login.html";
-    }
-
-    @PostMapping("/login")
-    public void login(String username, String password, HttpServletResponse response, Model model) throws Exception {
-        String message = null;
-        Optional<User> user = userRepository.findByUsernameAndPassword(username, password);
-        if(user.isPresent()) {
-            userSession.setUser(user.get());
-            response.sendRedirect("/");
-        } else {
-            userSession.setLoginFailed(true);
-            model.addAttribute("message", message);
-            response.sendRedirect("/login");
-        }
-    }
-
-    @GetMapping("/logout")
-    public void logout(HttpServletResponse response, Model model) throws  Exception {
-        userSession.setUser(null);
-        model.addAttribute("logoutMessage", "You have logged out successfully.");
-        response.sendRedirect("/");
-    }
-
-    @GetMapping("/signup")
-    public String getSignup(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "signup.html";
-    }
-
-    @PostMapping("/signup")
-    public String signup(User theUser, Model model) {
-        String message;
-        if(userRepository.existsByUsername(userSession.getUser().getUsername())) {
-            model.addAttribute("message", "Username taken.");
-            return "redirect:/signup";
-        }
-        userRepository.save(theUser);
-        return "redirect:";
-    }
-
-   @GetMapping("/browse")
-   public String browse(Model model) {
-       model.addAttribute("books", bookRepository.findAll());
-       return "browse.html";
-   }
-
-   @GetMapping("/admin/manage")
-   public String searchmembers(Model model, @RequestParam(defaultValue = "") String username) {
-       model.addAttribute("users", userRepository.findByUsernameLike("%"+ username +"%"));
-       return "searchmembers.html";
-   }
-
-   @PostMapping("/admin/addbook")
-   public String addBook(Book book) {
-       bookRepository.save(book);
-       return "searchmembers.html";
-   }
-
-   @GetMapping("/admin/addbook")
-   public String getAddBook() {
-       return "addbook.html";
-   }
-    @GetMapping("/search")
-    public String getSearch(Model model) {
-    }
-
     @GetMapping("/books")
     public String getSearch(@RequestParam (value = "search", required = false) String search, Model model) {
         model.addAttribute("title", "LMS - Search");
@@ -199,7 +129,7 @@ public class LibraryController {
             return "viewbooks.html";
         }
     }
-}
+
     @GetMapping("admin/manage/view-members/{id}")
     public String viewMember(@PathVariable String id, Model model) {
         if(userSession.getUser() == null) {
