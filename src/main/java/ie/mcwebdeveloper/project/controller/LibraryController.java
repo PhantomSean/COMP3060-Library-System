289f4283727a796e016e26a99fd19d5f9ddc122b
Sam O'Neill <sam.oneill1@ucdconnect.ie>
2020-03-20 01:07:35 +0000

    Common search now working

--- src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
+++ src/main/java/ie/mcwebdeveloper/project/controller/LibraryController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,9 +35,11 @@ public class LibraryController {
         return "index.html";
     }
 
-    @GetMapping("/search")
-    public String getSearch(Model model) {
+    @GetMapping("/books")
+    public String getSearch(@RequestParam (value = "search", required = false) String search, Model model) {
         model.addAttribute("title", "LMS - Search");
+        List<Book> books = bookRepository.findAllByTitleOrAuthorLike(search);
+        model.addAttribute("books", books);
         return "search.html";
     }
 
