e90f6f4520b506920ca36c9dbfc5520ec4185833
Sam <sam.oneill1@ucdconnect.ie>
2020-03-20 15:42:40 +0000

    Update BookRepository.java

--- src/main/java/ie/mcwebdeveloper/project/repositories/BookRepository.java
+++ src/main/java/ie/mcwebdeveloper/project/repositories/BookRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findById(long id);
     List<Book> findAllByUserid(long userid); // To find current loans
-    @Query("select b from Book b where lower(b.title) like lower(concat(?1,'%')) or lower(b.author) like lower(concat(?1,'%'))")
+    @Query("select b from Book b where lower(b.title) like lower(concat('%', ?1,'%')) or lower(b.author) like lower(concat('%', ?1,'%'))")
     List<Book> findAllByTitleOrAuthorLike(String author);
 }
